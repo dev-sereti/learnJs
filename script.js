60c233dbf1c7b37680ac25664f36da2e3d76d966
dev <devsereti@gmail.com>
2025-09-09 08:39:42 +0300

    object created with new keyword

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function carInfo(model,year,color){
     this.model=model;
     this.year=year;
     this.color=color;
-    this.car=carInfo(){
-        console.log('Hello, your car model is ${this.model} while the car year is ${this.year} and color ${this.color}')
+    this.car=function(){
+        console.log('Hello, your car model is ${this.model} while the car year is ${this.year} and color ${this.color}');
     };
 }
+const car1=new carInfo('BMW',2017,'Silver');
+car1.car();
