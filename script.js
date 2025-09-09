afae2f0e098bfbdb1559b5fb84adcb2b5efefb63
dev <devsereti@gmail.com>
2025-09-09 08:45:15 +0300

    constructor used

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-function carInfo(model,year,color){
-    this.model=model;
-    this.year=year;
-    this.color=color;
-    this.car=function(){
-        console.log('Hello, your car model is ${this.model} while the car year is ${this.year} and color ${this.color}');
-    };
+class CarInfo {
+    constructor(model, year, color) {
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+    car() {
+        console.log(`Hello, your car model is ${this.model} while the car year is ${this.year} and color ${this.color}`);
+    }
 }
-const car1=new carInfo('BMW',2017,'Silver');
+
+const car1 = new CarInfo('BMW', 2017, 'Silver');
 car1.car();
