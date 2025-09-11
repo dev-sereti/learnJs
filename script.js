d0a36d00c5bea4e6e057a49a561cea6945f2c2c9
dev <devsereti@gmail.com>
2025-09-11 08:56:44 +0300

    Output year.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
     // Year, month (0-indexed), day, hour, minute, second, millisecond
-const date = new Date(1672531200000); // Represents January 1, 2023, 00:00:00 UTC
-document.getElementById("todayDate").innerHTML=timestampDate;
+const date = new Date(); // Represents January 1, 2023, 00:00:00 UTC
+document.getElementById("todayDate").innerHTML=date;
 
-const year= date.getDate();
+const year= date.getFullYear();
 const month=date.getMonth();
 const day=date.getDay();
 const hour=date.getHours();
 const minutes=date.getMinutes()
 
-
-
+console.log(`Year: ${year}`);
+console.log(`Month: ${month}`);
