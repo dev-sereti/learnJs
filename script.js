bbad3ca5338db03c2366c54fb83a27f81ba7675c
dev <devsereti@gmail.com>
2025-09-11 08:52:31 +0300

    accessing date components in javascript

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
     // Year, month (0-indexed), day, hour, minute, second, millisecond
-const timestampDate = new Date(1672531200000); // Represents January 1, 2023, 00:00:00 UTC
+const date = new Date(1672531200000); // Represents January 1, 2023, 00:00:00 UTC
 document.getElementById("todayDate").innerHTML=timestampDate;
+
+const year= date.getDate();
+const month=date.getMonth();
+const day=date.getDay();
+const hour=date.getHours();
+const minutes=date.getMinutes()
+
+
+
