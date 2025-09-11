0cf128b32bd235a9661812e1f462a98b2801eb9a
dev <devsereti@gmail.com>
2025-09-11 09:22:39 +0300

    Math.ceil()

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ console.log(Math.abs(6.9));
 console.log(Math.floor(5.4));
 console.log(Math.floor(-5.4));
 
+/*  Math.ceil() */
+console.log(Math.ceil(5.99));   // Output: 6
+console.log(Math.ceil(-2.7));   // Output: -2 (rounds up towards zero)
+
+
+/* Math.trunc() */
+
+
+
