42e1c3d726e40c0412c60c859640be6b6693cf14
dev <devsereti@gmail.com>
2025-09-11 08:43:23 +0300

    specificDate

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const dateTime=new Date();
-document.getElementById("todayDate").innerHTML=dateTime.toDateString();
+const specificDate = new Date("2025-12-25T10:00:00"); // ISO 8601 format is recommended
+document.getElementById("todayDate").innerHTML=specificDate.toDateString();
