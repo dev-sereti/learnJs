0291539a51820f70205b8c1a5471f6634de59696
dev <devsereti@gmail.com>
2025-09-12 09:59:25 +0300

    F grade

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let yourScore=69;
-if(yourScore>=40){
-  document.getElementById("score").innerHTML=("Your Grade: D");
-}else if(yourScore>=50){
-  document.getElementById("score").innerHTML=("Your Grade: C");
+let yourScore=9;
+if(yourScore>=70){
+  document.getElementById("score").innerHTML=("Your Grade: A");
 }else if(yourScore>=60){
   document.getElementById("score").innerHTML=("Your Grade: B");
-}else if (yourScore>=70){
-  document.getElementById("score").innerHTML=("Your Grade: A");
+}else if(yourScore>=50){
+  document.getElementById("score").innerHTML=("Your Grade: C");
+}else if (yourScore>=40){
+  document.getElementById("score").innerHTML=("Your Grade: D");
 }
 else{
   document.getElementById("score").innerHTML=("Your Grade: F");
