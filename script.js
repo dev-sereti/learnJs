58f6f559d4d903cd891924b2ba055fbf38e1fdbc
dev <devsereti@gmail.com>
2025-09-12 09:38:58 +0300

    else statement

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let age = 25;
+let age = 5;
 if(age>=18){
-  document.getElementById("voteStatus").innerHTML=("You can vote");
+  document.getElementById("voteStatusTrue").innerHTML=("You can vote");
 }else{
   document.getElementById=("voteStatusFalse").innerHTML=("Too young to vote");
-};
+}
