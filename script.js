1fd8b1f7df61b5486c35621166038024b99a9d51
dev <devsereti@gmail.com>
2025-09-12 09:54:27 +0300

    Else if statement  yourScore>=70)

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let yourScore=69;
 if(yourScore>=40){
-  document.getElementById("score").innerHTML=("Your Grade: A");
+  document.getElementById("score").innerHTML=("Your Grade: D");
 }else if(yourScore>=50){
-  document.getElementById("score").innerHTML=("Your Grade: A");
+  document.getElementById("score").innerHTML=("Your Grade: C");
 }else if(yourScore>=60){
+  document.getElementById("score").innerHTML=("Your Grade: B");
+}else if (yourScore>=70){
   document.getElementById("score").innerHTML=("Your Grade: A");
 }
