9d0cfcf2d0e4441a13717233b608ec036487b57e
dev <devsereti@gmail.com>
2025-09-15 10:08:49 +0300

    while loop to check fuel level

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-let fuelLeve=0,astronauts=0,altitude=0;
+let fuelLevel=0,astronauts=0,altitude=0;
+while(fuelLevel<=5000 || fuelLevel>=30000){
+  fuelLevel=Number(prompt("Enter starting fuel level (must be >5000 and <30000):"));
+  console.log("Fuel level is valid" + fuelLevel);
+}
