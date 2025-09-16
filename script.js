4c30645f3e0d1fda0601d2179335bc8caf571db7
dev <devsereti@gmail.com>
2025-09-16 11:21:01 +0300

    private properties

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ class Person {
 const p = new Person("Kelvin");
 console.log(p.name);  // ✅ Accessible
 p.greet();            // ✅ Accessible
+
+
+class BankAccount {
+  #balance = 0; // private property
+
+  deposit(amount) {
+    this.#balance += amount;
+    console.log(`Deposited: ${amount}`);
+  }
+
+  getBalance() {
+    return this.#balance; // access private property inside class
+  }
+}
+
+const account = new BankAccount();
+account.deposit(100);
+console.log(account.getBalance()); // ✅ 100
+console.log(account.#balance);     // ❌ ERROR: Private field
