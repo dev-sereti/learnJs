93dd86d0b9c68e13a3a16294664d8e72f3b0b355
dev <devsereti@gmail.com>
2025-09-16 10:54:06 +0300

    output

--- script.js
+++ script.js
@@ -6,6 +6,3 @@ const person = {
 };
 
 person.greet(); 
-// "Hi, I'm undefined" 
-// because `this` in arrow functions does not bind to `person`
-// it takes `this` from the outer scope (likely `window`).
