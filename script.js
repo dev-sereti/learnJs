71aa11ab453c7e6b28313170493aa286d280f811
dev <devsereti@gmail.com>
2025-09-16 10:52:49 +0300

    this in a arrow function

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const person = {
   name: "Kelvin",
-  greet: function () {
-    console.log("Hello, my name is " + this.name);
+  greet: () => {
+    console.log("Hi, I'm " + this.name);
   }
 };
 
 person.greet(); 
-
+// "Hi, I'm undefined" 
+// because `this` in arrow functions does not bind to `person`
+// it takes `this` from the outer scope (likely `window`).
