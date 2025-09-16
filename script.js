19b8867ef4f9e8f4158537daaa47352434a0bfd6
dev <devsereti@gmail.com>
2025-09-16 11:24:40 +0300

    Protected

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Pub
+
 class Person {
   constructor(name) {
     this.name = name; // public property
@@ -12,6 +14,7 @@ const p = new Person("Kelvin");
 console.log(p.name);  // ✅ Accessible
 p.greet();            // ✅ Accessible
 
+// Private
 
 class BankAccount {
   #balance = 0; // private property
@@ -30,3 +33,28 @@ const account = new BankAccount();
 account.deposit(100);
 console.log(account.getBalance()); // ✅ 100
 console.log(account.#balance);     // ❌ ERROR: Private field
+
+
+//Protected
+
+
+class Vehicle {
+  constructor(type) {
+    this._type = type; // "protected" by convention
+  }
+
+  _describe() {
+    console.log(`This is a ${this._type}`);
+  }
+}
+
+class Car extends Vehicle {
+  showType() {
+    // accessing "protected" property/method in child
+    this._describe();
+  }
+}
+
+const c = new Car("Sedan");
+c.showType();    // ✅ Works: "This is a Sedan"
+console.log(c._type); // ⚠️ Possible, but not recommended
