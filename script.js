24caa2202d3af0dbdfa6db134ab78d7dc5d3bde4
dev <devsereti@gmail.com>
2025-09-17 10:24:08 +0300

    number

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+// Pub
+
+class Person {
+  constructor(name) {
+    this.name = name; // public property
+  }
+
+  greet() {          // public method
+    console.log(`Hello, my name is ${this.name}`);
+  }
+}
+
+const p = new Person("Kelvin");
+console.log(p.name);  // ✅ Accessible
+p.greet();            // ✅ Accessible
+
+// Private
+
+class BankAccount {
+  #balance = 0; // private property
+
+  deposit(amount) {
+    this.#balance += amount;
+    console.log(`Deposited: ${amount}`);
+  }
+
+  getBalance() {
+    return this.#balance; // access private property inside class
+  }
+}
+
+const account = new BankAccount();
+account.deposit(100);
+console.log(account.getBalance()); // 100
+console.log(account.#balance);     // ERROR: Private field
+
+
+//Protected
+
+
+class Vehicle {
+  constructor(type) {
+    this._type = type; // "protected" by convention
+  }
+
+  _describe() {
+    console.log(`This is a ${this._type}`);
+  }
+}
+
+class Car extends Vehicle {
+  showType() {
+    // accessing "protected" property/method in child
+    this._describe();
+  }
+}
+
+const c = new Car("Sedan");
+c.showType();    // Works: "This is a Sedan"
+console.log(c._type); // Possible, but not recommended
+
 
 
 // Typeof
