45e90dcdf6a50d5834c34f95379a1c191399a65a
dev-sereti <devsereti@gmail.com>
2025-09-18 11:45:47 +0300

    Object.keys

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-const fruits=[];
-fruits.push("Bananas","Mangoes","Oranges","34");
-console.log(fruits.length);
+fruits[5] = "mango";
+console.log(fruits[5]); // 'mango'
+console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
+console.log(fruits.length); // 6
