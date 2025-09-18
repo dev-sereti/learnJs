a0e6553e5cd0031f51daec24d408e2165f1f84ce
dev-sereti <devsereti@gmail.com>
2025-09-18 12:03:41 +0300

    forEach

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-const fruits=[];
-fruits.push("Bananas","Mangoes","Oranges","34");
-console.log(fruits.length);
+const numbers = [10, 20, 30, 40];
 
-fruits[5]="Apples";
-console.log(fruits[4]);
-console.log(Object.keys(fruits));
-
-fruits.length = 10;
-console.log(fruits); // ['banana', 'apple', 'peach', empty x 2, 'mango', empty x 4]
-console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
-console.log(fruits.length); // 10
-console.log(fruits[8]); // undefined
+numbers.forEach((num, index) => {
+  console.log(`Index: ${index}, Value: ${num}`);
+});
