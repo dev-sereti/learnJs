bc5884caa8d15ebc483bacea981bd7eeb3961499
dev-sereti <devsereti@gmail.com>
2025-09-18 11:56:10 +0300

    Increasing the length e

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ console.log(fruits.length);
 fruits[5]="Apples";
 console.log(fruits[4]);
 console.log(Object.keys(fruits));
+
+fruits.length = 10;
+console.log(fruits); // ['banana', 'apple', 'peach', empty x 2, 'mango', empty x 4]
+console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
+console.log(fruits.length); // 10
+console.log(fruits[8]); // undefined
