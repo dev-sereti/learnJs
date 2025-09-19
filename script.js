668d2bff681168f0412c7cb9cefcc6111e6eed20
dev <devsereti@gmail.com>
2025-09-19 10:01:12 +0300

    flat()

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const words = ["spray", "elite", "exuberant", "destruction", "present"];
-console.log(words.length);
+const arr1 = [0, 1, 2, [3, 4]];
 
-const result = words.filter((word) => word.length > 6);
+console.log(arr1.flat());
+// expected output: Array [0, 1, 2, 3, 4]
 
-console.log(result);
+const arr2 = [0, 1, [2, [3, [4, 5]]]];
 
+console.log(arr2.flat());
+// expected output: Array [0, 1, 2, Array [3, Array [4, 5]]]
+
+console.log(arr2.flat(2));
+// expected output: Array [0, 1, 2, 3, Array [4, 5]]
+
+console.log(arr2.flat(Infinity));
+// expected output: Array [0, 1, 2, 3, 4, 5]
