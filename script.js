cca58d0d736e6f5a5ee3baef8b8e573596ee3744
dev <devsereti@gmail.com>
2025-09-19 09:32:01 +0300

    copyWithin()

--- script.js
+++ script.js
@@ -9,12 +9,7 @@ console.log(grocery.toString());
 
 //copyWithin()
 
-const array = ["a", "b", "c", "d", "e"];
-
 // Copy to index 0 the element at index 3
-console.log(array.copyWithin(0, 3, 4));
-// Expected output: Array ["d", "b", "c", "d", "e"]
+console.log(fruits.copyWithin(0, 3, 4));
+console.log(vegetables.copyWithin(1, 3));
 
-// Copy to index 1 all elements from index 3 to the end
-console.log(array.copyWithin(1, 3));
-// Expected output: Array ["d", "d", "e", "d", "e"]
