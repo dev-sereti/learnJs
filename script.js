8f17500665a20b854c64e8984023d7cedd119b3e
dev <devsereti@gmail.com>
2025-09-22 11:27:15 +0300

    aCCESSED

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Accessing an element by its ID
-const paragraphElement=document.getElementById('header');
+const paragraphElement=document.getElementById('par1');
 
 // Accessing elements by class name
-const paragraphs=document.getElementsByClassName('paragraph');
+const paragraphs=document.getElementsByClassName('.par2');
 
 // Accessing elements by tag name
 const images=document.getElementsByTagName('img');
 
 //modify element content
 
-paragraphElement.innerHTML='How to modify element content'
+paragraphElement.innerHTML='Document Object Model (DOM) manipulation.'
+paragraphs.innerHTML='How to modify element content'
