06bbbf5148dedfa21a4c81435bf459185fafa1a4
dev <devsereti@gmail.com>
2025-09-22 11:08:16 +0300

    Accessing elements by class name

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 // Accessing an element by its ID
-
 const headerElement=document.getElementById('header');
+
+// Accessing elements by class name
+const paragraphs=document.getElementsByClassName('paragraph');
+
