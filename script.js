ff19628cf474ab0ae1b137480cfb4952301bbf22
dev <devsereti@gmail.com>
2025-09-22 11:44:34 +0300

    Properties vs attributes vs dataset

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-const frag = document.createDocumentFragment();
-for (let i = 0; i < 1000; i++) {
-  const li = document.createElement('li');
-  li.textContent = `Item ${i}`;
-  frag.appendChild(li);
-}
-document.getElementById('bigList').appendChild(frag);
+
 
 
 const card = document.getElementById('card');
