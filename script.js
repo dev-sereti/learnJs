ce904a62489aa59e066ae58537823f45b298d48b
dev <devsereti@gmail.com>
2025-09-24 09:08:31 +0300

    String Conversion

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-    function Car(make, model) {
-      this.make = make;
-      this.model = model;
-    }
+let value = true;
+alert(typeof value); // boolean
 
-    Car.prototype.startEngine = function() {
-      console.log(`${this.make} ${this.model} engine started.`);
-    };
-
-    const myCar = new Car('Toyota', 'Camry');
-    myCar.startEngine(); // Output: Toyota Camry engine started.
+value = String(value); // now value is a string "true"
+alert(typeof value); // string
