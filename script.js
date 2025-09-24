cffc485626bff6bf65785b12a2cd02834a02f462
dev <devsereti@gmail.com>
2025-09-24 09:03:52 +0300

    prototypes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function carModels(carObject){
-    carObject.make="Toyota";
+    function Car(make, model) {
+      this.make = make;
+      this.model = model;
+    }
 
-    const myCar={
-        make:"Mazda",
-        model:"Axela",
-        year:2017,
+    Car.prototype.startEngine = function() {
+      console.log(`${this.make} ${this.model} engine started.`);
     };
-    console.log(myCar.make);
 
-}
+    const myCar = new Car('Toyota', 'Camry');
+    myCar.startEngine(); // Output: Toyota Camry engine started.
